455bb70596e2d1d57e58e38493ca9f17a8a48de7
DeenoDev <deyanoklaasen@outlook.com>
2023-05-19 23:53:45 +0200

    Added logic to control aria attributes with javascript.

--- manage-landing-page-master/main.js
+++ manage-landing-page-master/main.js
@@ -2,5 +2,9 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click', () =>{
-    primaryNav.classList.toggle('opened');
+    primaryNav.hasAttribute("data-visible") 
+    ? navToggle.setAttribute("aria-expanded", "false")
+    : navToggle.setAttribute("aria-expanded", "true");
+    primaryNav.toggleAttribute("data-visible");
+
 });
